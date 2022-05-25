3cfa0ebb0522237d5701fc18bca2eef430fba67a
Hardik Sheth <hardik.sheth1717@live.com>
2022-05-25 07:25:40 +0530

    Learned splitting components and passing props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseDate = new Date().toISOString();
-    const expenseType = 'Bike Service';
-    const expenseAmount = 300.00;
-
+const ExpenseItem = props => {
     return (
-
         <div className='expense-item'>
-            <div>{expenseDate}</div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{expenseType}</h2>
-                <div className='expense-item__price'>$ {expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>$ {props.amount}</div>
             </div>
         </div>
     )
